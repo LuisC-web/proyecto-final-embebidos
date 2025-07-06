79843de0e9ffc8dfb28fdd62e055439b641c75d1
LuisC <72262647+LuisC-web@users.noreply.github.com>
2025-07-06 08:55:30 -0500

    Agregando demas endpoint al carro

---
+++ frontend/src/services/AutoServices.ts
@@ -0,0 +1,56 @@
+import { api } from "../api/api_base";
+
+export class AutoServices {
+  static async mover_adelante() {
+    try {
+      const { data } = await api.post("/api/mover", {
+        accion: "mover",
+        movimiento: {
+          direccion: "adelante",
+          distancia_cm: 30,
+        },
+      });
+      return data.ok;
+    } catch (error) {
+      console.log("Se produjo un error", error);
+    }
+  }
+  static async mover_atras() {
+    try {
+      const { data } = await api.post("/api/mover", {
+        accion: "mover",
+        movimiento: {
+          direccion: "atras",
+          distancia_cm: 20,
+        },
+      });
+      return data.ok;
+    } catch (error) {
+      console.log("Se produjo un error", error);
+    }
+  }
+  static async girar_izquierda() {
+    try {
+      const { data } = await api.post("/api/mover", {
+        accion: "girar",
+        direccion: "izquierda",
+        angulo: 90,
+      });
+      return data.ok;
+    } catch (error) {
+      console.log("Se produjo un error", error);
+    }
+  }
+  static async girar_derecha() {
+    try {
+      const { data } = await api.post("/api/mover", {
+        accion: "girar",
+        direccion: "derecha",
+        angulo: 45,
+      });
+      return data.ok;
+    } catch (error) {
+      console.log("Se produjo un error", error);
+    }
+  }
+}
--- frontend/src/view/MovimientoCarro.tsx
+++ frontend/src/view/MovimientoCarro.tsx
@@ -4,14 +4,36 @@ import {
   ArrowBigRight,
   ArrowBigUp,
 } from "lucide-react";
+import { AutoServices } from "../services/AutoServices";
+console.log(import.meta.env.VITE_API_URL);
 function MovimientoCarro() {
   return (
     <div>
       <div className="grid grid-cols-3 gap-4 grid-rows-2">
-        <ArrowBigUp className="size-10 col-start-2 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all" />
-        <ArrowBigLeft className="size-10 col-start-1 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all" />
-        <ArrowBigRight className="size-10 col-start-3 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all" />
-        <ArrowBigDown className="size-10 col-2 row-start-2 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all" />
+        <ArrowBigUp
+          className="size-10 col-start-2 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all"
+          onClick={async () => {
+            await AutoServices.mover_adelante();
+          }}
+        />
+        <ArrowBigLeft
+          className="size-10 col-start-1 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all"
+          onClick={async () => {
+            await AutoServices.girar_izquierda();
+          }}
+        />
+        <ArrowBigRight
+          className="size-10 col-start-3 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all"
+          onClick={async () => {
+            await AutoServices.girar_derecha();
+          }}
+        />
+        <ArrowBigDown
+          className="size-10 col-2 row-start-2 cursor-pointer hover:bg-white/20  hover:scale-105 transition-all"
+          onClick={async () => {
+            await AutoServices.mover_atras();
+          }}
+        />
       </div>
       <div></div>
     </div>
